47276bd89adffa46f2bd3dde5d2330ba44aaa133
sbohmann <github@yeoman.at>
2025-09-02 11:14:31 +0200

    Removed experimental code

--- graph.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <gtk/gtk.h>
-
-#define GRAPH_TYPE_WIDGET (basic_example_get_type ())
-G_DECLARE_FINAL_TYPE (BasicExample, basic_example, GRAPH, WIDGET, GtkWidget)
-
-GtkWidget * basic_example_new       (void);
